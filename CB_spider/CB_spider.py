60357f92cd7c9c49d6c124476372b49db138d428
wangxing <wang_xing@nec.cn>
2021-07-29 16:12:12 +0900

    modify: 应对集思录请求接口变更（需要Cookies数据），以及json数据机构变更

--- CB_spider/CB_spider.py
+++ CB_spider/CB_spider.py
@@ -133,17 +133,31 @@ if __name__ == "__main__":
     params = {'___jsl': 'LST___t='+str(LST_time)}
     payload = {'fprice':'',
                'tprice':'',
+               'curr_iss_amt':'',
                'volume':'',
                'svolume':'',
                'premium_rt':'',
                'ytm_rt':'',
                'rating_cd':'',
                'is_search':'N',
+               'market_cd':['shmb','shkc','szmb','szcy'],
                'btype':'',
                'listed':'Y',
+               'qflag':'N',
+               'sw_cd':'',
                'industry':'',
                'bond_ids':'',
                'rp':50,
                'page':1}
-    req = requests.post(url=target,params=params,data=payload,verify=False)
+    cookies={}
+    cookies['Hm_lpvt_164fe01b1433a19b507595a43bf58262']=str(int(time.time()))
+    cookies['Hm_lvt_164fe01b1433a19b507595a43bf58262']='1626921687,1627000170,1627017807,1627260573'
+    cookies['kbz_newcookie']='1'
+    cookies['kbzw__Session']='3f9tjgiprevqdrf1n5fd18k2n5'
+    cookies['kbzw__user_login']='7Obd08_P1ebax9aXuEr4Al0pFF4UBUgCIpakptzY1enY5unX1r6bl6mt2c3dobCap8Sup9mtk9KT1a6lmtucqMatlqiro6-Cq47p4tnH1peola6VqaiUs47FotLWoLbo5uDO4sKmrKGogZi43efZ2PDfl7DKgainoaickLjd56udtIzvmKqcl-npspqgj6SilbDez-LRpZOnqKOokqCSlL_e297S5tqlmqelow..'
+    username='A股没法玩'
+    name=username.encode("utf-8").decode('latin1') #文字utf8编码
+    cookies['kbzw_r_uname']=name
+    requests.packages.urllib3.disable_warnings() #消除https证书缺失警告
+    req = requests.post(url=target,params=params,data=payload,cookies=cookies,verify=False)
     StatisticsJson(req.json())
