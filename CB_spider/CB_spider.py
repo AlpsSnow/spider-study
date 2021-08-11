d3ca4606026961f77f5f88646c937a94edf1ba36
wangxing <wang_xing@nec.cn>
2021-08-11 18:14:07 +0900

    add: 添加可转债投资星级功能

--- CB_spider/CB_spider.py
+++ CB_spider/CB_spider.py
@@ -5,13 +5,19 @@ import requests
 import time
 import json
 
+def get_median(data):
+    data.sort()
+    half = len(data) // 2 #向下取接近商的整数
+    return (data[half] + data[~half]) / 2
+
+
 def get_two_float(f_str, n):
     f_str = str(f_str)
     a, b, c = f_str.partition('.')
     c = (c+"0"*n)[:n]
     return ".".join([a, c])
 
-def StatisticsJson(jsondate):
+def analyzeJson(jsondate):
 
     cb_total = 0
 
@@ -56,6 +62,7 @@ def StatisticsJson(jsondate):
     total = jsondate['total']
     total_list = jsondate['rows']
     items = iter(total_list)
+    cb_list=[]
     for item in items:
         name = item['cell']['bond_nm']
         if name[2] == '转':
@@ -66,7 +73,8 @@ def StatisticsJson(jsondate):
                 ytm = float(0)
             premium = float(item['cell']['premium_rt'].split('%')[0])
             price = float(item['cell']['price'])
-            rating = item['cell']['rating_cd']            
+            cb_list.append(price)
+            rating = item['cell']['rating_cd']
             if rating == 'AAA':
                 cb_3A_list.append(item)
                 cb_3A_count = cb_3A_count + 1
@@ -114,6 +122,7 @@ def StatisticsJson(jsondate):
                     cb_Larger125_min_premium_name = name
 
     print('{} 可转债价格统计： {}'.format('-'*20,'-'*20))
+    print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     print('可转债个数：', cb_total)
     print('跌破90元,占比：{}%（历史最低：占11%）'.format(get_two_float(cb_Less90_count*100/cb_total, 2))) 
     print('跌破95元,占比：{}%（历史最低：占18%）'.format(get_two_float((cb_Less90_count+cb_Less95_count)*100/cb_total, 2)))
@@ -124,7 +133,29 @@ def StatisticsJson(jsondate):
         cb_3A_max_ytm_cb['cell']['price'], cb_3A_max_ytm_cb['cell']['ytm_rt'], cb_3A_max_ytm_cb['cell']['premium_rt']))
     print('溢价率最小的3A可转债: {}，价格：{}，ytm:{}，溢价率：{}，'.format(cb_3A_min_premium_cb['cell']['bond_nm'],
         cb_3A_min_premium_cb['cell']['price'], cb_3A_min_premium_cb['cell']['ytm_rt'], cb_3A_min_premium_cb['cell']['premium_rt'])) 
+    print()
 
+    median = get_median(cb_list)
+    starStr = ''
+    if median >= 120:
+        starStr = '★'
+    elif median >= 115 and median < 120:
+        starStr = '★★'
+    elif median >= 110 and median < 115:
+        starStr = '★★★'
+    elif median >= 100 and median < 1110:
+        starStr = '★★★★'
+    else:
+        starStr = '★★★★★'
+    print('{} 可转债投资星级： {}'.format('*'*20,'*'*20))
+    print('{}星级{}估值{}中位价格{}建议仓位'.format('|-','-|-','|-','-|'))
+    print('{}一星{}高估{}120以上{}20%'.format('|-','-|-','|-','-|'))
+    print('{}二星{}偏高{}115-120{}30%'.format('|-','-|-','|-','-|'))
+    print('{}三星{}合理{}110-115{}50%'.format('|-','-|-','|-','-|'))
+    print('{}四星{}偏低{}100-110{}70%'.format('|-','-|-','|-','-|'))
+    print('{}五星{}低估{}100以下{}30%'.format('|-','-|-','|-','-|'))
+
+    print('中位价格：{}，投资星级：{}'.format(str(median),starStr))
     return True
 
 if __name__ == "__main__":
@@ -151,13 +182,13 @@ if __name__ == "__main__":
                'page':'1'}
     cookies={}
     cookies['Hm_lpvt_164fe01b1433a19b507595a43bf58262']=str(LST_time)
-    cookies['Hm_lvt_164fe01b1433a19b507595a43bf58262']='1626921687,1627000170,1627017807,1627260573'
+    cookies['Hm_lvt_164fe01b1433a19b507595a43bf58262']=str(int(time.time()))+','+str(int(time.time()))+','+str(int(time.time()))+','+str(int(time.time()))
     cookies['kbz_newcookie']='1'
-    cookies['kbzw__Session']='3f9tjgiprevqdrf1n5fd18k2n5'
+    cookies['kbzw__Session']='a0jojn8bhsjb4jf1jo4nr3rgb2'
     cookies['kbzw__user_login']='7Obd08_P1ebax9aXuEr4Al0pFF4UBUgCIpakptzY1enY5unX1r6bl6mt2c3dobCap8Sup9mtk9KT1a6lmtucqMatlqiro6-Cq47p4tnH1peola6VqaiUs47FotLWoLbo5uDO4sKmrKGogZi43efZ2PDfl7DKgainoaickLjd56udtIzvmKqcl-npspqgj6SilbDez-LRpZOnqKOokqCSlL_e297S5tqlmqelow..'
     username='A股没法玩'
     name=username.encode("utf-8").decode('latin1') #文字utf8编码
     cookies['kbzw_r_uname']=name
     requests.packages.urllib3.disable_warnings() #消除https证书缺失警告
     req = requests.post(url=target,params=params,data=payload,cookies=cookies,verify=False)
-    StatisticsJson(req.json())
+    analyzeJson(req.json())
